6f177906d27a32c882dca1d4cb1e87db6d4f885b
Brandon Colangelo <brandoncancode@gmail.com>
2023-05-02 09:38:27 -0400

    Initial Commit

---
+++ index.py
@@ -0,0 +1,23 @@
+# Given an array, print and return the max, min and average values for that array.
+
+def max_min_avg(list):
+    max = 0
+    min = list[0]
+    sum = 0
+
+    for i in range(0,len(list)):
+        if(list[i] > max):
+            max = list[i]
+
+        elif(list[i] < min):
+            min = list[i]
+
+        sum = sum + list[i]
+        avg = sum/len(list)
+
+    print(f"The maximum is: {max}, the minimum is: {min}, and the average is: {int(avg)}")
+
+
+
+print(max_min_avg([2,5,2]))
+
